0409d4a091c458457d7aa02f6a32926ddd6148f5
Diana Nazarova <alllleksandrova@mail.ru>
2023-06-16 21:22:42 +0400

    feature: add movie's model

--- src/movie/model/movie.js
+++ src/movie/model/movie.js
@@ -2,18 +2,67 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 
 const movieSchema = new mongoose.Schema({
-  country: {},
-  director: {},
-  duration: {},
-  year: {},
-  description: {},
-  image: {},
-  trailerLink: {},
-  thumbnail: {},
-  owner: {},
-  movieId: {},
-  nameRU: {},
-  nameEN: {},
+  country: {
+    required: true,
+    type: String,
+  },
+  director: {
+    required: true,
+    type: String,
+  },
+  duration: {
+    required: true,
+    type: Number,
+  },
+  year: {
+    required: true,
+    type: String,
+  },
+  description: {
+    required: true,
+    type: String,
+  },
+  image: {
+    required: true,
+    type: String,
+    validate: {
+      validator: (url) => validator.isURL(url),
+      message: 'Введите URL',
+    },
+  },
+  trailerLink: {
+    required: true,
+    type: String,
+    validate: {
+      validator: (url) => validator.isURL(url),
+      message: 'Введите URL',
+    },
+  },
+  thumbnail: {
+    required: true,
+    type: String,
+    validate: {
+      validator: (url) => validator.isURL(url),
+      message: 'Введите URL',
+    },
+  },
+  owner: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  movieId: {
+    required: true,
+    type: Number,
+  },
+  nameRU: {
+    required: true,
+    type: String,
+  },
+  nameEN: {
+    required: true,
+    type: String,
+  },
 });
 
 module.exports = mongoose.model('movie', movieSchema);
