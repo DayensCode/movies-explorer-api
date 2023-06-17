f3f9e4301b62beacef691d136425fb2fb2750269
Diana Nazarova <alllleksandrova@mail.ru>
2023-06-17 13:47:28 +0400

    feature: add user's controller

--- src/user/controller/userController.js
+++ src/user/controller/userController.js
@@ -1,2 +1,34 @@
 const User = require('../model/user');
+const NotFoundError = require('../../error/not-found-error');
 
+function getUserInfo(req, res, next) {
+  const { _id } = req.user;
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('Пользователь не найден');
+      } else {
+        res.send(user);
+      }
+    })
+    .catch(next);
+}
+
+function updateUserInfo(req, res, next) {
+  const { email, name } = req.body;
+  const { _id } = req.user;
+  User.findByIdAndUpdate(_id, { email, name }, { new: true, runValidators: true })
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('Пользователь не найден');
+      } else {
+        res.send(user);
+      }
+    })
+    .catch(next);
+}
+
+module.exports = {
+  getUserInfo,
+  updateUserInfo,
+};
