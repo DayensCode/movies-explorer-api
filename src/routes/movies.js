699689485371af9717470eedba115153fa17f186
Diana Nazarova <alllleksandrova@mail.ru>
2023-06-21 12:38:00 +0400

    feature: add movie's validation

--- src/routes/movies.js
+++ src/routes/movies.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
+const { createMovieValidation, deleteMovieValidation } = require('../movie/validation/validation');
 const { getMovies, createMovie, deleteMovie } = require('../movie/controller/movieController');
 
 router.get('/', getMovies);
-router.post('/', createMovie);
-router.delete('/:id', deleteMovie);
+router.post('/', createMovieValidation, createMovie);
+router.delete('/:id', deleteMovieValidation, deleteMovie);
 
 module.exports = router;
